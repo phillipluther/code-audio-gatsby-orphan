6b28ea1f609ad0729153f8dda372510a097f02fc
Phillip Luther <dev@phillipluther.com>
2021-11-12 07:11:23 -0800

    Add SVG component support; layout updates with initial nav menus

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,9 +1,63 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import classnames from 'classnames';
 import VisuallyHidden from '../visually-hidden';
-// import Logo from './dilettante-guru-logo.svg';
+import Logo from './dilettante-guru-logo.svg';
 import styles from './layout.module.css';
-import utilStyles from '../../styles/utils.module.css';
+// import utilStyles from '../../styles/utils.module.css';
+
+const navLinks = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+  {
+    href: '/about',
+    label: 'About',
+  },
+  {
+    href: '/contact',
+    label: 'Contact',
+  },
+];
+
+const socialLinks = [
+  {
+    href: 'https://twitter.com/DilettanteGuru',
+    label: '@DilettanteGuru on Twitter',
+  },
+];
+
+const NavLinks = ({ className }: {
+  className?: string,
+}) => (
+  <nav className={classnames(styles.nav, className)}>
+    <ul>
+      {navLinks.map(({ href, label }) => (
+        <li key={href}>
+          <Link href={href}>
+            <a>
+              {label}
+              <VisuallyHidden>{' '}The Dilettante Guru</VisuallyHidden>
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+const SocialLinks = ({ className }: {
+  className?: string,
+}) => (
+  <ul className={classnames(styles.social, className)}>
+    {socialLinks.map(({ href, label }) => (
+      <li key={href}>
+        <a href={href}>{label}</a>
+      </li>
+    ))}
+  </ul>
+);
 
 export default function Layout({ children, home }: {
   children: React.ReactNode,
@@ -21,11 +75,17 @@ export default function Layout({ children, home }: {
         <TitleTag className={styles.title}>
           <Link href="/">
             <a>
-              {/* <Logo className={styles.logo} aria-hidden="true" /> */}
+              <Logo
+                className={styles.logo}
+                aria-hidden="true"
+              />
               <VisuallyHidden>The Dilettante Guru</VisuallyHidden>
             </a>
           </Link>
         </TitleTag>
+
+        <NavLinks />
+        <SocialLinks />
       </header>
 
       <main>{children}</main>

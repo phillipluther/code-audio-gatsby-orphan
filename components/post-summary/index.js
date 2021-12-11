a603c1bb15d316daa3874b1404fdd5951f7dd8e0
Phillip Luther <dev@phillipluther.com>
2021-12-11 04:44:51 -0800

    On second thought, Gatsby 4

--- components/post-summary/index.js
+++
@@ -1,1 +0,0 @@
-export { default, PostProps } from './post-summary';
--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from 'next/app';
-import '@fontsource/lora';
-import '@fontsource/lora/600.css';
-import '../styles/global.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
